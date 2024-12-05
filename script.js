df29fb4261a57854cb1792b68d16010fc9dc417e
scojr <180873402+scojr@users.noreply.github.com>
2024-12-04 18:36:48 -0600

    Remove this keyword from module functions

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const gameBoard = (function () {
   const grid = [];
 
   function makeColumn() {
-    let column1 = "";
-    let column2 = "";
-    let column3 = "";
+    let column1 = "[]";
+    let column2 = "[]";
+    let column3 = "[]";
     return [column1, column2, column3];
   }
 
@@ -14,14 +14,15 @@ const gameBoard = (function () {
   }
 
   function drawBoard() {
-    console.log(this.grid[0])
-    console.log(this.grid[1])
-    console.log(this.grid[2])
+    console.log(grid[0])
+    console.log(grid[1])
+    console.log(grid[2])
   }
 
   function placeMarker(marker, x, y) {
-    this.grid[x][y] = marker;
+    grid[x][y] = marker;
   }
 
-  return { grid, drawBoard, placeMarker };
+  return { drawBoard, placeMarker };
 })();
+
